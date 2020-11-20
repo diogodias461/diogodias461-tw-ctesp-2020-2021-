41746da5dcdc00b53eac0177a8fc11ccc534bd29
diogodias461 <diasdiogo461@gmail.com>
2020-11-20 09:11:14 +0000

    ficha js 1

---
+++ js/java.js
@@ -0,0 +1,9 @@
+let name="João";
+let apelido="Silva";
+let nomeCompleto = name + " " + apelido;
+console.log(nomeCompleto)
+
+let numero1= 1;
+let numero2= 3;
+let resultado = numero1 + numero2 + name;
+console.log(resultado);
